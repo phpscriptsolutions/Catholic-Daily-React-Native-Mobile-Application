2539f5d723bd1655a1169085f0a4d65f0f5c83e1
Anthony Okonta <okontaa@gmail.com>
2019-08-29 23:40:58 +0300

    About fix

--- Components/AppNavigation.js
+++ Components/AppNavigation.js
@@ -8,7 +8,7 @@ class About extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
+        <Text>Preaching the Gospel through technology!</Text>
       </View>
     );
   }
